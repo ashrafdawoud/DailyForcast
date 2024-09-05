63d8a4668d8f5a1e50e2e7db32b4a823973a9422
dawoud <ashrafdawoud1111@gmail.com>
2024-09-05 21:04:34 +0300

    Revert "feat: implement data layer"
    This reverts commit b542f45e8590cdd86e14ad8cc74b7f368da468ed.

--- app/src/main/java/com/baims/dailyforcast/data/remote/datasource/ForecastDataRemoteDataSource.kt
+++
@@ -1,8 +0,0 @@
-package com.baims.dailyforcast.data.remote.datasource
-
-import com.baims.dailyforcast.data.remote.dto.WeatherResponseDto
-import retrofit2.Response
-
-interface ForecastDataRemoteDataSource {
-    suspend fun getForecastData(lat: Double, lon: Double): Response<WeatherResponseDto>
-}
